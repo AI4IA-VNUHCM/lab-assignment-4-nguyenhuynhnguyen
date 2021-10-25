f50e3120248fa9003d2051ecff5212b3b4414ffe
nguyenhuynhnguyen <91869289+nguyenhuynhnguyen@users.noreply.github.com>
2021-10-25 14:24:22 +0700

    Update Exercise2.c

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -4,7 +4,7 @@ Sort the odd column in increasing order and the
 even column in decreasing order.
 Ex:
 ___________________________________________________________________________________
-| Input: 5(row) 4(col) 10 21 12 53 64 53 86 72 68 99 10 11 12 13 14 15 16 17 18 19 |
+| Input: 5 4 10 21 12 53 64 53 86 72 68 99 10 11 12 13 14 15 16 17 18 19 |
 | The converted 2D array will be like this:                                        |
 | 10 21 12 53                                                                      |
 | 64 53 86 72                                                                      |
@@ -22,52 +22,105 @@ ________________________________________________________________________________
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #define SIZE 100
 
 void Array2Dconverter(int arr[], int a[SIZE][SIZE], int m, int n)
 {
-	int row, column;
-	int counter = 0;
-	//Convert 1D array to 2D array
-	for (row = 0; row <= (m - 1); row ++){
-		for (column = 0; column <= (n - 1); column ++){
-			a[row][column] = arr[counter];
-			counter++;
-		}
-	}
+  int row, column;
+  int counter = 0;
+  //Convert 1D array to 2D array
+  for (row = 0; row <= (m - 1); row++)
+  {
+    for (column = 0; column <= (n - 1); column++)
+    {
+      a[row][column] = arr[counter];
+      counter++;
+    }
+  }
 }
 
 void printArray(int a[SIZE][SIZE], int m, int n)
 {
-	int row, column;
+  int row, column;
 
-	for (row = 0; row <= (m - 1); row ++){
-		for (column = 0; column <= (n - 1); column ++){
-			printf("%d ", a[row][column]);
-		}
-		printf("\n");
-	}
+  for (row = 0; row <= (m - 1); row++)
+  {
+    for (column = 0; column <= (n - 1); column++)
+    {
+      printf("%d ", a[row][column]);
+    }
+    printf("\n");
+  }
 }
 
-void Ex2(int arr[], int m, int n){
-	int a[SIZE][SIZE];
-	Array2Dconverter(arr,a,m,n);
-	//Your codes here
+void swap(int *xp, int *yp)
+{
+  int temp = *xp;
+  *xp = *yp;
+  *yp = temp;
+}
+// ij
+// 00 01 02 03
+// 10 11 12 13
+void Ex2(int arr[], int m, int n)
+{
+  int a[SIZE][SIZE];
+  Array2Dconverter(arr, a, m, n);
+  //Your codes here
 
-	printArray(a, m, n);
+  for (int i = 0; i < m; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      // printf("%d%d ", i, j);
+
+      if (j % 2 == 0 || j == 0)
+      {
+        for (int i1 = 0; i1 < m; i1++)
+        {
+          for (int j1 = 0; j1 < n; j1++)
+          {
+            if (a[i1][j1] < a[i1 + 1][j1])
+            {
+              swap(&a[i1][j1], &a[i1 + 1][j1]);
+            }
+          }
+        }
+      }
+      else
+      {
+        for (int i1 = 0; i1 < m; i1++)
+        {
+          for (int j1 = 0; j1 < n; j1++)
+          {
+            if (a[i1 - 1][j1] > a[i1][j1])
+            {
+              swap(&a[i1][j1], &a[i1 - 1][j1]);
+            }
+          }
+        }
+      }
+    }
+    // printf("\n");
+  }
+
+  printArray(a, m, n);
 }
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	int row = atoi(argv[1]);
-	int col = atoi(argv[2]);
-	argc-=3;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		testcase[i] = atoi(argv[i+3]);
-	}
+int main(int argc, char *argv[])
+{
+  //testing variable, applying it to your algorithm for auto-evaluating
+  int row = atoi(argv[1]);
+  int col = atoi(argv[2]);
+  argc -= 3;
+  int testcase[argc], i;
+  for (i = 0; i < argc; i++)
+  {
+    testcase[i] = atoi(argv[i + 3]);
+  }
 
-	Ex2(testcase, row, col);
-	
-	return 0;
+  Ex2(testcase, row, col);
+
+  return 0;
 }
